971ef6b89dbdc817605d2f4f6d675f9b3804176f
Sathya Ganesan <sathyaganesan.b2w@gmail.com>
2021-01-06 13:30:32 -0500

    Sql Quries are done

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -1,1 +1,17 @@
 // scheme-model
+const db = require("../../data/db-config");
+
+function find() {
+    return db("schemes")
+        .select("*"); 
+}
+
+function findById(id) {
+    return db("schemes")
+        .select("*")
+        .where("schemes.id");
+}
+module.exports = {
+    find,
+    findById
+}
